33702d105d672d85723cbe31f0d0583e3fadb268
Maulana, Hardani <hardani.maulana@tugraz.at>
2021-06-23 09:48:32 +0000

    Update environment.prod.ts using tu graz keycloak

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,9 +1,16 @@
 import { KeycloakConfig } from 'keycloak-js';
-
+/*
 const keycloakConfig: KeycloakConfig = {
   url: $KEYCLOAK.DAMAP_KEYCLOAK_PROD,
   realm: $KEYCLOAK.DAMAP_REALM_PROD,
   clientId: $KEYCLOAK.DAMAP_CLIENT_ID_PROD
+};
+*/
+
+const keycloakConfig: KeycloakConfig = {
+  url: 'https://auth-test.tugraz.at/auth',
+  realm: 'tugraz',
+  clientId: 'dmap-test_tugraz_at'
 };
 
 export const environment = {
